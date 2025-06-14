97ddf70dbfa3c71ebc41f066a2ad84ded9f67bc9
Karim <amanbyword@gmail.com>
2025-06-14 12:44:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/*Objects */
+class CounterObj {
+    constructor(id, name, tab, total) {
+        this.id = id;
+        this.name = name;
+        this.tab = tab;
+        this.total = total;
+    }
+}
+/*End of Objects */
+
+[
+    new CounterObj(1, "A", 1, 0),
+    new CounterObj(2, "B", 2, 0),
+    new CounterObj(3, "C", 1, 0),
+]
